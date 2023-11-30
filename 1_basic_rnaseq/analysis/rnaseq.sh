2cd0a21321fd81bd33056a7aad23b4056dd63e4b
utsumidaisuke <utsumidaisuke@hotmail.com>
2023-11-30 21:42:39 +0900

    changed

---
+++ 1_basic_rnaseq/analysis/rnaseq.sh
@@ -0,0 +1,19 @@
+# directory setting
+mkdir qc trimmed_fastq
+
+# quality control
+fastqc -t 4 -o qc ../../fastq/$1\_1.fastq.gz ../../fastq/$1\_2.fastq.gz
+
+# trimming
+trim_galore -j 20 --paired ../../fastq/$1\_1.fastq.gz ../../fastq/$1\_2.fastq.gz -o trimmed_fastq
+
+# mapping
+hisat2 -p 16 -x ../../index/human_genome -1 trimmed_fastq/$1\_1_val_1.fq.gz -2 trimmed_fastq/$1\_2_val_2.fq.gz -S $1\.sam 
+
+# convert sam to bam
+samtools sort $1\.sam -@ 10 -O bam -o $1\.sort.bam 
+samtools index $1\.sort.bam
+rm $1\.sam
+
+# read counting
+stringtie -p 10 -G ../../ref/ncbi_dataset/data/GCF_000001405.40/genomic.gff -o $1\.gtf -A  $1\.table $1\.sort.bam
